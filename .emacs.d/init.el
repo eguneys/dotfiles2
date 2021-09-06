a35587ddbb063de017812a801ca5a8357e391f40
Emre Guneyler <eguneys@gmail.com>
2021-09-06 20:56:21 +0300

    no backup files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,25 @@
 
 (require 'whole-line-or-region)
 
+
+;; no backup files
+;; get rid of backup and autosave files
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+	(,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+
+
 (load "defuns-config.el")
 (global-set-key "\C-h" 'backward-delete-char)
 (global-set-key "\C-n" 'next-line)
